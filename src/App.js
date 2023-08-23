e48d9a5e5f22aecce3787fd5ed7b0113ff1e6bad
shuati <ouyangyly@gmail.com>
2023-08-23 20:34:02 +0800

    chat bot interact with chatgpt and LIama

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  organization: "org-0nmrFWw6wSm6xIJXSbx4FpTw",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    if (!message) return;
+    setIsTyping(true);
+    scrollTo(0, 1e10);
+
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+    setMessage("");
+    await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a EbereGPT. You can help with graphic design tasks",
+          },
+          ...chats,
+        ],
+      })
+      .then((res) => {
+        msgs.push(res.data.choices[0].message);
+        setChats(msgs);
+        setIsTyping(false);
+        scrollTo(0, 1e10)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <main>
+      <h1>Chat AI Tutorial</h1>
+
+      <section>
+        {chats && chats.length
+          ? chats.map((chat, index) => (
+            <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
+              <span>
+                <b>{chat.role.toUpperCase()}</b>
+              </span>
+              <span>:</span>
+              <span>{chat.content}</span>
+            </p>
+          ))
+          : ""}
+      </section>
+
+      <div className={isTyping ? "" : "hide"}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <i>{isTyping ? "Typing" : ""}</i>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+
+      <form action="" onSubmit={(e) => chat(e, message)}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Type a message here and hit Enter..."
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </main>
   );
 }
 
